aa51bfe44f0aac463632edeff30df9ec03daa934
Melanie De Giusti <meludegiusti@Melanies-MacBook-Pro.local>
2021-09-12 16:03:25 -0400

    ver mas y menos

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,21 +1,48 @@
-import React from 'react'
+import React, {Component} from 'react';
 import "./card.css"
+import { render } from 'react-dom';
 
-function Card(props) {
-    console.log(props)
-    const {poster_path, title, overview, release_date, original_language} = props.datosPelicula
-    return(
-        <div className='movie-card'>
-            <img src={`https://image.tmdb.org/t/p/w342/${poster_path}`} /> 
-            <div className="info">
-            <h4>{title}</h4>
-            <p>{overview}</p>
-            <p>Idioma: {original_language}</p>
-            <p>Fecha de estreno: {release_date}</p>
-            <p className="more">Ver más</p>
+class Card extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewMore: false,
+            text:'Ver más',
+        }
+    }
+
+    viewMore(evento){
+        if (this.state.viewMore) {
+            this.setState({
+                viewMore: false,
+                text: 'Ver más'
+            });
+        } else{
+            this.setState({
+                viewMore: true,
+                text: 'Ver menos'
+            });
+        }
+
+        
+    }
+    
+    render() {
+        return(
+            <div className='movie-card'>
+                <img src={`https://image.tmdb.org/t/p/w342/${this.props.datosPelicula.poster_path}`} /> 
+                <div className="info">
+                <h4>{this.props.datosPelicula.title}</h4>
+                <p>{this.props.datosPelicula.overview}</p>
+                <div className={ this.state.viewMore ? '' : 'hide'}>
+                <p>Idioma: {this.props.datosPelicula.original_language}</p>
+                <p>Fecha de estreno: {this.props.datosPelicula.release_date}</p>
+                </div>
+                <p className="more" onClick={()=> this.viewMore()}>{this.state.text}</p>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Card;
